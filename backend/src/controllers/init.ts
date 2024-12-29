a351d3ef9683230a7047bc75bcc1cefbcc3f58c8
duhnush <dhanusu006@gmail.com>
2024-12-29 22:51:55 +0530

    init file

--- backend/src/controllers/init.ts
+++ backend/src/controllers/init.ts
@@ -1,3 +1,18 @@
+import { promises as fs } from "fs";
+import path from "path";
+
 export const initRepo = async () => {
-  console.log("Initializing a new repo");
+  const repoPath = path.resolve(process.cwd(), ".mygit");
+  const commitsPath = path.join(repoPath, "commits");
+  try {
+    await fs.mkdir(repoPath, { recursive: true });
+    await fs.mkdir(commitsPath, { recursive: true });
+    await fs.writeFile(
+      path.join(repoPath, "config.json"),
+      JSON.stringify({ bucket: process.env.S3_BUCKET })
+    );
+    console.log("Initialized repo");
+  } catch (error) {
+    console.error("Error initializing repo", error);
+  }
 };

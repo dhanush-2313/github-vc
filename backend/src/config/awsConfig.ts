eab2535f74c652e1e369dc2c3b06b5af170ca4b2
duhnush <dhanusu006@gmail.com>
2025-01-01 23:36:30 +0530

    s3 push working

--- backend/src/config/awsConfig.ts
+++ backend/src/config/awsConfig.ts
@@ -3,7 +3,15 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-AWS.config.update({ region: "ap-south-1" });
+console.log("AWS_ACCESS_KEY_ID:", process.env.AWS_ACCESS_KEY_ID);
+console.log("AWS_SECRET_ACCESS_KEY:", process.env.AWS_SECRET_ACCESS_KEY);
+console.log("S3_BUCKET:", process.env.S3_BUCKET);
+
+AWS.config.update({
+  region: "ap-south-1",
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+});
 
 const s3 = new AWS.S3();
 const s3Bucket = process.env.S3_BUCKET;
